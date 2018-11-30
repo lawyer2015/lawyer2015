1a5e8cd51d601075286ff1f5cda680b44cc4d065
Mostafa Mahmoud <must345@yahoo.com>
2018-11-30 02:41:43 +0200

    moo

---
+++ lawyer2015/lawyer2015/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lawyer2015.MvcApplication" Language="C#" %>
---
+++ lawyer2015/lawyer2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lawyer2015.Startup))]
+namespace lawyer2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
